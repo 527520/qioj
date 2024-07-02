be6492da4ec10bf4ac395f73361f3e415eecfb90
wuqian <2194961543@qq.com>
2024-07-02 22:44:16 +0800

    新建项目

---
+++ src/main/java/com/wqa/qiojcodesandbox/QiojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.wqa.qiojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wqa/qiojcodesandbox/QiojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wqa.qiojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
